b3a6fd1d2c5df99b3c6c2026cb639f662fd63e10
minamikik <mia@sync.dev>
2022-12-16 04:03:46 +0900

    Update package

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .model import get_model
-from .defaults import _C as cfg
-from .dataset import expand_bbox
-
-__all__ = ["get_model", "cfg", "expand_bbox"]
---
+++ age_estimation/__init__.py
@@ -0,0 +1,5 @@
+import age_model
+import age_dataset
+import age_defaults
+
+__all__ = ["age_model", "age_dataset", "age_defaults"]
