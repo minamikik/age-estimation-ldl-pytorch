3df5ce6f8696709664ae183cac5db2a6b7d5392c
minamikik <mia@sync.dev>
2023-02-12 06:22:21 +0900

    Update age_predictor.py

--- age_estimation/age_predictor.py
+++ age_estimation/age_predictor.py
@@ -12,33 +12,24 @@ from facenet_pytorch import MTCNN
 from .age_model import get_model
 from .age_dataset import expand_bbox
 from .age_defaults import _C as config
-from logging import getLogger
-logger = getLogger(__name__)
-logger.level = 20
 
 class AgePredictor:
     def __init__(self, model_path):
-        logger.debug('AgePredictor: Initializing')
         self.model_path = model_path
 
         try:
-            if os.path.exists(model_path):
-                logger.debug(f'AgePredictor: Loading model from {model_path}')
-            else:
+            if not os.path.exists(model_path):
                 os.makedirs(os.path.dirname(model_path), exist_ok=True)
                 url = 'https://github.com/yjl450/age-estimation-ldl-pytorch/releases/download/v1.0/megaage_fusion.pth'
-                logger.debug(f'AgePredictor: Downloading model {model_path}')
                 r = requests.get(url, allow_redirects=True)
                 open(model_path, 'wb').write(r.content)
 
             self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-            logger.debug(f'AgePredictor: Using device {self.device}')
 
             self.model = get_model(model_name=config.MODEL.ARCH, pretrained=None)
 
             self.model.eval()
             self.model = self.model.to(self.device)
-            logger.debug(f'AgePredictor: Model loaded')
 
             checkpoint = torch.load(self.model_path, map_location="cpu")
             self.model.load_state_dict(checkpoint['state_dict'])
@@ -46,7 +37,6 @@ class AgePredictor:
             self.mtcnn = MTCNN(device=self.device, post_process=False, keep_all=False)
             self.img_size = config.MODEL.IMG_SIZE
         except Exception as e:
-            logger.debug(f'AgePredictor: Error initializing: {e}')
             raise e
 
 
@@ -81,7 +71,6 @@ class AgePredictor:
             torch.cuda.empty_cache()
             return age
         except Exception as e:
-            logger.debug(f'AgePredictor: Error predicting age: {e}')
             torch.cuda.empty_cache()
             raise e
 
