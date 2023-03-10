f7ee473acbc97965c63cf7c77d465a7a2848a233
minamikik <mia@sync.dev>
2023-03-11 05:07:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,15 @@ setup(
     packages=find_packages(),
     install_requires=[
         'torch',
+        'torchvision',
         'numpy',
         'tqdm',
+        'pretrainedmodels',
+        'albumentations',
+        'facenet_pytorch',
+        'pandas',
+        'Pillow',
+        'matplotlib'
+
     ],
 )
